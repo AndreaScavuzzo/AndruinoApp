16687e0c5f624adc382bcc38c938db99495a018e
AndreaScavuzzo <andrea.scavuzzo@gmail.com>
2016-03-16 21:53:07 +0100

    Update ANDRUINO_0DEFINES.h

--- Andruino_Base_Sketch/ANDRUINO_0DEFINES.h
+++ Andruino_Base_Sketch/ANDRUINO_0DEFINES.h
@@ -16,15 +16,15 @@
 
 
 //RF MODULES
-#define NRF24L_ENABLE               1                               //NRF MODULES ENABLE
+#define NRF24L_ENABLE               0                               //NRF MODULES ENABLE
 #define ZIGBEE_ENABLE               0                               //enable the ZIGBEE module receiver (1)
 
 
 //Sensors
 #define Dallas_DS18B20              0                               //Enable OneWire library for DallasTemperature DS18B20
 #define THERMO_ADAFRUIT_MAX31855    0                               //THERMOCOUPLE ADAFRUIT K MAX31855 (Not validated)
-#define POWER_CONSUMPTION_ENABLE    1                               //enable the ADC conversion for Irms calculation on adc0/adc1  (Validated, ok)
-#define THERMO_ADAFRUIT_DHT         1                               //FIRST DHT sensor
+#define POWER_CONSUMPTION_ENABLE    0                               //enable the ADC conversion for Irms calculation on adc0/adc1  (Validated, ok)
+#define THERMO_ADAFRUIT_DHT         0                               //FIRST DHT sensor
 #define THERMO_ADAFRUIT_DHT2        0                               //SECOND DHT sensor
 
 
